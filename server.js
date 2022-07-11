b388c5571ac0ea77d648443bb3221e89f4ea1e6a
Sunti <saknsaza87@gmail.com>
2022-07-11 11:39:12 +0700

    update

--- server.js
+++ server.js
@@ -8,22 +8,56 @@ app.use(cors());
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/api/test', (req, resp) => {
-    let data = {}
+app.post('/api/Shopsakainfo', (req, resp) => {
     try {
         return resp.status(200).json(
             {
                 status: "ok",
-                data: "test"
+                data: data
             }
         )
-    }catch (error) {
+    } catch (error) {
         return resp.status(500).json(
             {
                 status: "fail",
-                data: "test"
             }
         )
     }
 
 })
+
+
+
+
+
+var data = [[{
+    "customerID": "00388",
+    "shopName": "เลิฟมีเทนเดอร์",
+    "branch": "สำนักงานใหญ่ 1",
+    "username": "admin123",
+    "status": "open",
+    "branch_detail": "การทำน้อยกว่าที่ทำย้อมดีกว่าไม่ทำอะไรเลย",
+    "tel": "08x-xxxx-xxx",
+    "typeShope": "{\"storeFront\":\"yes\",\"typeShope\":\"ร้านขายเครื่องสำอาง\"}"
+},
+{
+    "customerID": "00388",
+    "shopName": "เลิฟมีเทนเดอร์",
+    "branch": "สำนักงานใหญ่ 2",
+    "username": "admin23",
+    "status": "open",
+    "branch_detail": "การทำน้อยกว่าที่ทำย้อมดีกว่าไม่ทำอะไรเลย",
+    "tel": "06x-xxxx-xxx",
+    "typeShope": "{\"storeFront\":\"yes\",\"typeShope\":\"ร้านขายเครื่องสำอาง\"}"
+},
+{
+    "customerID": "00388",
+    "shopName": "เลิฟมีเทนเดอร์",
+    "branch": "สำนักงานใหญ่ 3",
+    "username": "admin3",
+    "status": "open",
+    "branch_detail": "การทำน้อยกว่าที่ทำย้อมดีกว่าไม่ทำอะไรเลย",
+    "tel": "09x-xxxx-xxx",
+    "typeShope": "{\"storeFront\":\"yes\",\"typeShope\":\"ร้านขายเครื่องสำอาง\"}"
+}
+]]
